a7949015d4bd965d3497cf539c2c2200cb573b79
bella908 <mumtahina efa8@gmail.com>
2024-08-17 05:45:03 +0600

    firebase

---
+++ src/FireBase/FireBase.int.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0-hZ1fvhhcxXUXOMsCilZdxMdWpiEjIo",
+  authDomain: "makeup-web-29572.firebaseapp.com",
+  projectId: "makeup-web-29572",
+  storageBucket: "makeup-web-29572.appspot.com",
+  messagingSenderId: "782065959891",
+  appId: "1:782065959891:web:132be7dfb4a5f3fc94e526"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
